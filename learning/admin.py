c38b234a49d6382568414a25e97644dd30d565ed
nurulanisa5519-png <nurulanisa5519@gmail.com>
2026-08-07 00:28:18 +0800

    Update admin.py

--- learning/admin.py
+++ learning/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
-
 from .models import (
     Profile,
     Semester,
@@ -9,90 +7,41 @@ from .models import (
     Mahasiswa,
     MataKuliah,
     Course,
-
 )
 
-# =========================
-# User & Group
-# =========================
-
-admin.site.unregister(User)
-admin.site.register(User)
-
-admin.site.unregister(Group)
-admin.site.register(Group)
-
-# =========================
-# Profile
-# =========================
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "role")
-    search_fields = ("user__username",)
 
-# =========================
-# Semester
-# =========================
 
 @admin.register(Semester)
 class SemesterAdmin(admin.ModelAdmin):
     list_display = ("id", "nama")
 
-# =========================
-# Kelas
-# =========================
 
 @admin.register(Kelas)
 class KelasAdmin(admin.ModelAdmin):
     list_display = ("id", "nama")
 
-# =========================
-# Dosen
-# =========================
 
 @admin.register(Dosen)
 class DosenAdmin(admin.ModelAdmin):
     list_display = ("id", "nama", "nip")
-    search_fields = ("nama", "nip")
 
-# =========================
-# Mahasiswa
-# =========================
 
 @admin.register(Mahasiswa)
 class MahasiswaAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "nama",
-        "nim",
-        "kelas",
-    )
+    list_display = ("id", "nama", "nim", "kelas")
 
-    search_fields = (
-        "nama",
-        "nim",
-    )
-
-# =========================
-# Mata Kuliah
-# =========================
 
 @admin.register(MataKuliah)
 class MataKuliahAdmin(admin.ModelAdmin):
-    list_display = (
-        "kode",
-        "nama",
-        "sks",
-    )
+    list_display = ("kode", "nama", "sks")
 
-# =========================
-# Course
-# =========================
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-
     list_display = (
         "id",
         "mata_kuliah",
@@ -101,8 +50,8 @@ class CourseAdmin(admin.ModelAdmin):
         "dosen",
     )
 
-    list_filter = (
-        "semester",
-        "kelas",
-    )
+
+
+
+
 
